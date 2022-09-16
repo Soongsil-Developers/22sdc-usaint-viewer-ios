14f1d77bfd185e0b5bf83110200b61397b204501
shlim0 <46235301+shlim0@users.noreply.github.com>
2022-09-16 19:49:33 +0900

    Delete TextAlertView.swift

--- Usaint_viewer_ios/TextAlertView.swift
+++
@@ -1,9 +0,0 @@
-//import Foundation
-//import UIKit
-//import SwiftUI
-//
-//struct TextAlertView: UIViewControllerRepresentable{
-//    func makeUIViewController(context: UIViewControllerRepresentable<TextAlertView>) -> UIViewController {
-//
-//    }
-//}
